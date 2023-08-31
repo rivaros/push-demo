7932aea928a865102f66ec86785b0fa637e5942f
ross <1242369+rivaros@users.noreply.github.com>
2023-08-31 04:38:48 +0400

    push notifications set up

--- ios/PushDemoProject/AppDelegate.mm
+++ ios/PushDemoProject/AppDelegate.mm
@@ -10,6 +10,23 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  
+  [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+  UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert | UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
+  [[UNUserNotificationCenter currentNotificationCenter]
+   requestAuthorizationWithOptions:authOptions
+   completionHandler:^(BOOL granted, NSError * _Nullable error) {
+    NSLog(@"DEMO: Permission check");
+    if (granted) {
+      NSLog(@"DEMO: Permission was granted");
+//        dispatch_async(dispatch_get_main_queue(), ^{
+//            [[UIApplication sharedApplication] registerForRemoteNotifications];
+//        });
+    } else {
+      NSLog(@"DEMO: Permission was not granted: %@", error.description);
+    }
+  }];
+  
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
@@ -22,5 +39,26 @@
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
+
+
+-(void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler
+{
+  // Get the content of the received notification
+  UNNotificationContent *notificationContent = notification.request.content;
+  // Extract the user info of the notification
+  NSDictionary *userInfo = notificationContent.userInfo;
+
+  // Braze
+  if (@available(iOS 14.0, *)) {
+    if (userInfo[@"aps"][@"mutable-content"]) {
+      completionHandler(UNNotificationPresentationOptionList | UNNotificationPresentationOptionBanner);
+    } else {
+      completionHandler(UNNotificationPresentationOptionAlert);
+    }
+  } else {
+    completionHandler(UNNotificationPresentationOptionAlert);
+  }
+}
+
 
 @end
